d2211c592133c99e218ac7b1bc37ce395992c3f8
irfana-IT <122743680+irfana-IT@users.noreply.github.com>
2024-01-31 17:55:58 +0000

    variables_concept

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountID= 7788147852
+let accEmail= "Talabh@gmail.com"
+var accPasword= "12345678"
+accCity= "Newyork"
+
+//accountID= 99668527418
+/* 
+
+we cannot re-assign the const_value again,If we do so it gives us error
+2.to declare a variable in js,we should always use let ...
+
+*/
+console.log(accEmail);
+accCity="Itly"
+console.table([accountID,accEmail,accPasword,accCity])
+
